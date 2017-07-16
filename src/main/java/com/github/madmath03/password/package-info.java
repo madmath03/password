4b27a46a7700cf8b006f7f9cdc3a5bc69025a37b
madmath03 <mb.mathieu.brunot@gmail.com>
2017-07-16 15:43:29 +0200

    Fixed reporting issues and code coverage reports.

---
+++ src/main/java/com/github/madmath03/password/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Creation by madmath03 the 2017-07-16.
+ */
+
+/**
+ * A simple package for managing passwords and check passwords VS hashed values.
+ * 
+ * @author madmath03
+ */
+package com.github.madmath03.password;
